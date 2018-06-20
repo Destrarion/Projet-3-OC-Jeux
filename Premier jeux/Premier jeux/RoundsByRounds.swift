535b697a6f41791039c8da330f3732f4ce3fc18d
Fabien Dietrich <destrarion@gmail.com>
2018-06-20 11:43:36 +0200

    Ajout des atks des Trois personnage dans la fonction RoundAtkFunction.

--- Premier jeux/Premier jeux/RoundsByRounds.swift
+++ Premier jeux/Premier jeux/RoundsByRounds.swift
@@ -19,13 +19,57 @@ func RoundAtkFuction () {
     if let choiceAtk = readLine(){
         switch choiceAtk {
         case "1" : player1.listOfCharacter[0].atkfunction(target: player2.listOfCharacter[0], caster: player1.listOfCharacter[0])
-        print("\(player2.listOfCharacter[0].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[0].name) got \(player2.listOfCharacter[0].lifepoint)")
+        print("\(player2.listOfCharacter[0].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[0].name) got \(player2.listOfCharacter[0].lifepoint)lifepoint.")
             
         case "2" : player1.listOfCharacter[0].atkfunction(target: player2.listOfCharacter[1], caster: player1.listOfCharacter[0])
-        print("\(player2.listOfCharacter[1].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[1].name) got \(player2.listOfCharacter[1].lifepoint)")
+        print("\(player2.listOfCharacter[1].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[1].name) got \(player2.listOfCharacter[1].lifepoint)lifepoint.")
             
         case "3" : player1.listOfCharacter[0].atkfunction(target: player2.listOfCharacter[2], caster: player1.listOfCharacter[0])
-        print("\(player2.listOfCharacter[2].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[2].name) got \(player2.listOfCharacter[2].lifepoint)")
+        print("\(player2.listOfCharacter[2].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[2].name) got \(player2.listOfCharacter[2].lifepoint)lifepoint.")
+            
+        default:
+            print("You haven't choose someone to attack. You must atk one of your ennemy with")
+            RoundAtkFuction()
+        }
+    }
+    // here the N°2 of the list
+    print("Which ennemy want to atk with \(player1.listOfCharacter[1])?"
+        + "\n1. \(player2.listOfCharacter[0].name)"
+        + "\n2. \(player2.listOfCharacter[1].name)"
+        + "\n3. \(player2.listOfCharacter[2].name)")
+    
+    if let choiceAtk = readLine(){
+        switch choiceAtk {
+        case "1" : player1.listOfCharacter[1].atkfunction(target: player2.listOfCharacter[0], caster: player1.listOfCharacter[1])
+        print("\(player2.listOfCharacter[0].name) as taken \(player1.listOfCharacter[1].strenghtAtk) and now" + "\(player2.listOfCharacter[0].name) got \(player2.listOfCharacter[0].lifepoint)lifepoint.")
+            
+        case "2" : player1.listOfCharacter[1].atkfunction(target: player2.listOfCharacter[1], caster: player1.listOfCharacter[1])
+        print("\(player2.listOfCharacter[1].name) as taken \(player1.listOfCharacter[1].strenghtAtk) and now" + "\(player2.listOfCharacter[1].name) got \(player2.listOfCharacter[1].lifepoint)lifepoint.")
+            
+        case "3" : player1.listOfCharacter[1].atkfunction(target: player2.listOfCharacter[2], caster: player1.listOfCharacter[1])
+        print("\(player2.listOfCharacter[2].name) as taken \(player1.listOfCharacter[1].strenghtAtk) and now" + "\(player2.listOfCharacter[2].name) got \(player2.listOfCharacter[2].lifepoint)lifepoint.")
+            
+        default:
+            print("You haven't choose someone to attack. You must atk one of your ennemy with")
+            RoundAtkFuction()
+        }
+    }
+    // And the 3rd
+    print("Which ennemy want to atk with \(player1.listOfCharacter[2])?"
+        + "\n1. \(player2.listOfCharacter[0].name)"
+        + "\n2. \(player2.listOfCharacter[1].name)"
+        + "\n3. \(player2.listOfCharacter[2].name)")
+    
+    if let choiceAtk = readLine(){
+        switch choiceAtk {
+        case "1" : player1.listOfCharacter[2].atkfunction(target: player2.listOfCharacter[0], caster: player1.listOfCharacter[2])
+        print("\(player2.listOfCharacter[0].name) as taken \(player1.listOfCharacter[2].strenghtAtk) and now" + "\(player2.listOfCharacter[0].name) got \(player2.listOfCharacter[0].lifepoint)lifepoint.")
+            
+        case "2" : player1.listOfCharacter[2].atkfunction(target: player2.listOfCharacter[1], caster: player1.listOfCharacter[2])
+        print("\(player2.listOfCharacter[1].name) as taken \(player1.listOfCharacter[0].strenghtAtk) and now" + "\(player2.listOfCharacter[1].name) got \(player2.listOfCharacter[1].lifepoint)lifepoint.")
+            
+        case "3" : player1.listOfCharacter[2].atkfunction(target: player2.listOfCharacter[2], caster: player1.listOfCharacter[2])
+        print("\(player2.listOfCharacter[2].name) as taken \(player1.listOfCharacter[2].strenghtAtk) and now" + "\(player2.listOfCharacter[2].name) got \(player2.listOfCharacter[2].lifepoint)lifepoint.")
             
         default:
             print("You haven't choose someone to attack. You must atk one of your ennemy with")
